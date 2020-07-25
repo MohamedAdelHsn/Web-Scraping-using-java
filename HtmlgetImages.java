65678c73af0a4526df12b719de7708981e370ec9
Mohamed Adel Hassan <58120325+MohamedAdelHsn@users.noreply.github.com>
2020-07-25 08:33:30 +0200

    Update HtmlgetImages.java

--- HtmlgetImages.java
+++ HtmlgetImages.java
@@ -12,12 +12,13 @@ public class HtmlgetImages {
 
 	public static void main(String[] args) throws IOException {
 		
-		  System.out.print("Enter yor url : ");
-		  String url = new Scanner(System.in).nextLine();
-		  Document doc = Jsoup.connect(url).get();   
-		  // define filter to match images in html
-		  Elements images = doc.select("img[src~=(?i)\\.(png|jpe?g|gif)]");  
-          for (Element image : images) {  
+	      System.out.print("Enter yor url : ");
+	      String url = new Scanner(System.in).nextLine();
+	      Document doc = Jsoup.connect(url).get();   
+              // define filter to match images in html
+	      Elements images = doc.select("img[src~=(?i)\\.(png|jpe?g|gif)]");  
+		
+              for (Element image : images) {  
         	  
               System.out.println("src : " + image.attr("src"));   
               System.out.println("alt : " + image.attr("alt"));  
